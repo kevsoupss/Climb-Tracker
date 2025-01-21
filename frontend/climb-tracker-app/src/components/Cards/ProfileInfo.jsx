1f0232c2f78b51d57ae5f733a3bab75f7342c367
Kevin Lei <kevinleimc@gmail.com>
2025-01-21 00:25:41 -0800

    added Profile section

--- frontend/climb-tracker-app/src/components/Cards/ProfileInfo.jsx
+++ frontend/climb-tracker-app/src/components/Cards/ProfileInfo.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import {getInitials} from "../../utils/helper"
 
-const ProfileInfo = () => {
+
+const ProfileInfo = ({ onLogout}) => {
   return (
-    <div>
-      ProfileInfo
+    <div className="flex items-center gap-3">
+        <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100 hover:bg-slate-200 transition">
+            {getInitials("Sam Woo")}
+        </div>
+
+
+        <div>
+            <p className="text-sm font-medium">Kevin</p>
+            <button className="text-sm text-slate-700 underline" onClick={onLogout}>
+                Logout
+            </button>
+        </div>
     </div>
   )
 }
