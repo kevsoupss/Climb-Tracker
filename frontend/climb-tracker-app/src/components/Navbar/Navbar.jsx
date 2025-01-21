3d7c4448ba017784ca24ebe181822e76d6ffd8d3
Kevin Lei <kevinleimc@gmail.com>
2025-01-20 22:58:47 -0800

    finished signup page

--- frontend/climb-tracker-app/src/components/Navbar/Navbar.jsx
+++ frontend/climb-tracker-app/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ProfileInfo from '../Cards/ProfileInfo'
 
 const Navbar = () => {
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
         <h2 className="text-xl font-medium text-black py-2">Climb Tracker</h2>
+        <ProfileInfo />
     </div>
   )
 }
--- frontend/climb-tracker-app/src/pages/SignUp/SignUp.jsx
+++ frontend/climb-tracker-app/src/pages/SignUp/SignUp.jsx
@@ -1,15 +1,38 @@
 import React, {useState} from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import PasswordInput from '../../components/Input/PasswordInput'
+import { Link } from "react-router-dom"
+import {validateEmail} from "../../utils/helper"
 
 const SignUp = () => {
 
 
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
-  const[password, setPassword] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState(null)
 
   const handleSignUp = async (e) => {
     e.preventDefault();
+
+    if (!name) {
+      setError("Please enter your name")
+      return
+    }
+
+    if (!validateEmail(email)){
+        setError("Please enter a valid email address.")
+        return
+    }
+    
+    if (!password) {
+        setError("Please enter a password")
+        return
+    }
+    
+    
+    setError("")
+    
 
   }
   
@@ -24,11 +47,34 @@ const SignUp = () => {
                
 
                 <input type="text" 
-                placeholder="Email" 
-                className="input-box" 
-                //value={email} 
-                //onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Name" 
+                  className="input-box" 
+                  value={name} 
+                  onChange={(e) => setName(e.target.value)}
                  /> 
+
+                 <input type="text" 
+                  placeholder="Email" 
+                  className="input-box" 
+                  value={email} 
+                  onChange={(e) => setEmail(e.target.value)}
+                 /> 
+
+                <PasswordInput 
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+               />
+            <button type="submit" className="btn-primary" > Create Account</button>
+
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+            <p className="text-sm text-center mt-4">
+              Already have an account?{" "}
+              <Link to="/Login" className="font-medium text-primary underline">
+                Login
+              </Link>
+            </p>
+
+            
             </form>
             </div>
           </div>
