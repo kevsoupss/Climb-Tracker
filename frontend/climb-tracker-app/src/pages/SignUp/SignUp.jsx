78412f5669b17d417237a5781528279b91c7f737
Kevin Lei <kevinleimc@gmail.com>
2025-01-20 18:36:24 -0800

    first commit/added a few components and frontend design

---
+++ frontend/climb-tracker-app/src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+
+const SignUp = () => {
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+  }
+  
+  return (
+    <>
+      <Navbar />
+
+        <div className="flex items-center justify-center mt-28">
+          <div className="w-96 border rounded bg-white px-7 py-10"> 
+            <form onSubmit={handleSignUp}>
+                <h4 className="text-2xl mb-7">Sign Up</h4>
+               
+
+                <input type="text" 
+                placeholder="Email" 
+                className="input-box" 
+                //value={email} 
+                //onChange={(e) => setEmail(e.target.value)}
+                 /> 
+            </form>
+            </div>
+          </div>
+        </>
+  )
+}
+
+export default SignUp
