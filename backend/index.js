eca609b26612017e5e2113c300a18b498a4d2b79
Kevin Lei <kevinleimc@gmail.com>
2025-02-23 19:34:20 -0800

    env change

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 const config = require("./config.json")
 const mongoose = require("mongoose")
 
-mongoose.connect(config.connectionString)
+mongoose.connect(process.env.connectionString)
 
 const User = require("./models/user.model")
 const Climb = require("./models/climb.model")
